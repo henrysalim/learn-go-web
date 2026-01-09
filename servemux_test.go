b3e0961c853d80a1c6c2cd0cb3cb93d047a11f2a
henrysalim <henrysalim22@gmail.com>
2026-01-09 14:56:49 +0700

    serve mux

---
+++ servemux_test.go
@@ -0,0 +1,27 @@
+package learngoweb
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World")
+	})
+	mux.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hi")
+	})
+
+	server := http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
