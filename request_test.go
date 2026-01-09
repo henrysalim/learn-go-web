a5cbcbd47751bb0bacdb21f9a5eecfa3abf401d0
henrysalim <henrysalim22@gmail.com>
2026-01-09 15:01:34 +0700

    request

---
+++ request_test.go
@@ -0,0 +1,24 @@
+package learngoweb
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestRequest(t *testing.T) {
+	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, r.Method)
+		fmt.Fprintln(w, r.RequestURI)
+	}
+
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
